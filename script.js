ff84ead40c602b946a2d3d5f137a7c2236bd539d
Lau-zjg <l.krueger.0205@gmail.com>
2026-09-15 23:50:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,12 @@ function showNextSlide() {
 
 setInterval(showNextSlide, 7000);
 
+let currentGardenImages = [];
+let currentGardenImageIndex = 0;
+
 function openGardenModal(
   titel,
-  bild,
+  bilder,
   status,
   groesse,
   laube,
@@ -30,8 +33,15 @@ function openGardenModal(
   frei
 ) {
 
+  // Falls nur ein Bild angegeben wurde
+  if (!Array.isArray(bilder)) {
+    bilder = [bilder];
+  }
+
+  currentGardenImages = bilder;
+  currentGardenImageIndex = 0;
+
   document.getElementById("modalTitle").innerText = titel;
-  document.getElementById("modalImage").src = bild;
   document.getElementById("modalStatus").innerText = status;
   document.getElementById("modalSize").innerText = groesse;
   document.getElementById("modalLaube").innerText = laube;
@@ -39,7 +49,57 @@ function openGardenModal(
   document.getElementById("modalStrom").innerText = strom;
   document.getElementById("modalFrei").innerText = frei;
 
+  updateGardenImage();
+
   document.getElementById("gardenModal").style.display = "block";
+}
+
+function updateGardenImage() {
+
+  document.getElementById("modalImage").src =
+    currentGardenImages[currentGardenImageIndex];
+
+  const counter = document.getElementById("modalCounter");
+  const prevButton = document.getElementById("gardenPrev");
+  const nextButton = document.getElementById("gardenNext");
+
+  // Bildnummer anzeigen
+  counter.innerText =
+    (currentGardenImageIndex + 1) +
+    " / " +
+    currentGardenImages.length;
+
+  // Pfeile nur anzeigen, wenn es mehrere Bilder gibt
+  if (currentGardenImages.length > 1) {
+    prevButton.style.display = "block";
+    nextButton.style.display = "block";
+    counter.style.display = "block";
+  } else {
+    prevButton.style.display = "none";
+    nextButton.style.display = "none";
+    counter.style.display = "none";
+  }
+}
+
+function changeGardenImage(direction) {
+
+  currentGardenImageIndex += direction;
+
+  // Vom letzten Bild wieder zum ersten
+  if (currentGardenImageIndex >= currentGardenImages.length) {
+    currentGardenImageIndex = 0;
+  }
+
+  // Vom ersten Bild wieder zum letzten
+  if (currentGardenImageIndex < 0) {
+    currentGardenImageIndex = currentGardenImages.length - 1;
+  }
+
+  updateGardenImage();
+}
+
+function closeGardenModal() {
+  document.getElementById("gardenModal").style.display = "none";
 }
 
 function closeGardenModal() {
